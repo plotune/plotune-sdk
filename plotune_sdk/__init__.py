b36732679a36d2b220f018c573a1be0b0c99dccd
kantrveysel <kantrveysel@gmail.com>
2025-12-30 09:30:59 +0300

    version update

--- plotune_sdk/__init__.py
+++ plotune_sdk/__init__.py
@@ -1,6 +1,6 @@
 """Plotune SDK package initialization."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from plotune_sdk.src import PlotuneRuntime, FormLayout
 from plotune_sdk.utils import AVAILABLE_PORT
